25b7777db46ce6eebb04d2a39be9376e6d182ab6
Blair McBride <bmcbride@mozilla.com>
2014-10-18 01:40:33 +1300

    Add form for becoming a member. No backend implementation yet.

---
+++ routes/views/join.js
@@ -0,0 +1,17 @@
+var keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+  var view = new keystone.View(req, res),
+			locals = res.locals;
+
+	locals.section = 'join';
+	locals.formData = req.body || {};
+	locals.validationErrors = {};
+
+	view.on('post', { action: 'join' }, function(next) {
+
+	});
+
+	view.render('join');
+
+};
