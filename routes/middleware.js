8b6edb60dab0001286fd8fec3aa5fdf966480964
James Macfie <james@macfie.co.nz>
2016-10-12 18:34:46 +1300

    Log errors to the console

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,8 +1,8 @@
 /**
  * This file contains the common middleware used by your routes.
- * 
+ *
  * Extend or replace these functions as your application requires.
- * 
+ *
  * This structure is not enforced, and just a starting point. If
  * you have more middleware you may want to group it as separate
  * modules in your project's /lib directory.
@@ -15,7 +15,7 @@ var _ = require('underscore'),
 
 /**
 	Initialises the standard view locals
-	
+
 	The included layout depends on the navLinks array to generate
 	the navigation in the header, you may wish to change this array
 	or replace it with your own templates / logic.
@@ -44,6 +44,7 @@ exports.initLocals = function(req, res, next) {
 
 exports.initErrorHandlers = function(req, res, next) {
 	res.err = function(err) {
+		console.log('ERROR!', err);
 		res.status(500).render('errors/500', {
 			error: err
 		});
