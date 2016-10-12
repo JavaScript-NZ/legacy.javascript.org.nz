67f4fb43a2be150afc20a80c7ccd3d212bd2dcc4
James Macfie <james@macfie.co.nz>
2016-10-12 18:36:00 +1300

    Use user model for membership signup

--- models/User.js
+++ models/User.js
@@ -9,19 +9,19 @@ var keystone = require('keystone'),
 var User = new keystone.List('User');
 
 User.add({
-	name: { type: Types.Name, required: true, index: true },
+	name: { type: String, required: true, index: true },
 	email: { type: Types.Email, initial: true, required: true, index: true },
 	password: { type: Types.Password, initial: true, required: true },
 	groups: { type: Types.Relationship, ref: 'UserGroup', many: true },
 	registeredOn: { type: Types.Date, default: Date.now, noedit: true },
 }, 'Membership', {
 	membershipType: { type: Types.Select, options: 'unpaid, student, regular', required: true, emptyOption: false, default: 'unpaid' },
-	paidUntil: { type: Types.Date },
+	paidUntil: { type: Date, default: new Date().setYear(new Date().getFullYear() + 1) }
 }, 'Profile', {
 	visibility: { type: Types.Select, options: [{value: 0, label: 'Committee only'}, {value: 1, label: 'Members only'}, {value: 2, label: 'Public'}], required: true, emptyOption: false, default: 0, initial: true },
 }, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
-	committeeRole: { type: Types.Select, options: 'officer, treasurer, secretary, president' }
+	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true, default: false },
+	committeeRole: { required: false, type: Types.Select, options: 'officer, treasurer, secretary, president, member' }
 });
 
 // Provide access to Keystone
@@ -35,6 +35,32 @@ User.schema.virtual('canAccessKeystone').get(function() {
  */
 
 User.relationship({ ref: 'Post', path: 'author' });
+
+
+/**
+ * Schema methods
+ */
+
+User.schema.methods.sendCommitteeEmail = function(callback) {
+	console.log('sendCommitteeEmail');
+	var user = this;
+
+	var recipientQuery = keystone.list('User').model.find();
+
+	recipientQuery.exec(function(err) {
+		if (err) return callback(err);
+
+		new keystone.Email('user-membership').send({
+			to: 'james@macfie.co.nz',
+			from: {
+				name: 'JavaScript NZ',
+				email: 'contact@javascript.org.nz'
+			},
+			subject: 'JavaScript NZ membership request',
+			enquiry: user
+		}, callback);
+	});
+}
 
 
 /**
--- routes/views/join.js
+++ routes/views/join.js
@@ -1,17 +1,85 @@
-var keystone = require('keystone');
+const keystone = require('keystone');
+const User = keystone.list('User');
+
+/**
+ * Rules and Conduct aren't part of the user model so have to be validated
+ * seperately. Returns values that can be used to mimic the return validation
+ * object from a model update.
+ */
+function validateAcceptance(agreeRules, agreeConduct) {
+	const errors = {};
+	const messages = [];
+
+	if (!agreeRules) {
+		messages.push('Please agree to the JavaScript NZ Rules')
+		errors.agreeRules = {
+			path: 'agreeRules'
+		};
+	}
+
+	if (!agreeConduct) {
+		messages.push('Please agree to the JavaScript NZ Code of Conduct');
+		errors.agreeConduct = {
+			path: 'agreeConduct'
+		};
+	}
+
+	if (!messages.length) {
+		return;
+	}
+
+	return {
+		errors,
+		messages
+	};
+}
 
 exports = module.exports = function(req, res) {
-  var view = new keystone.View(req, res),
+	const view = new keystone.View(req, res),
 			locals = res.locals;
 
 	locals.section = 'join';
 	locals.formData = req.body || {};
 	locals.validationErrors = {};
+	locals.joinSubmitted = false;
 
-	view.on('post', { action: 'join' }, function(next) {
+	view.on('post', { action: 'join' }, function joinPost(next) {
+		const newUser = new User.model(Object.assign({}, req.body, {
+			password: 'dummyPassword',
+			agreeRules: req.body.agreeRules !== undefined,
+			agreeConduct: req.body.agreeConduct !== undefined
+		}));
+		const updater = newUser.getUpdateHandler(req);
 
+		// Check agreement validation seperately from the user validation
+		const agreeRules = req.body.agreeRules === 'on';
+		const agreeConduct = req.body.agreeConduct === 'on';
+		const agreementValidation = validateAcceptance(agreeRules, agreeConduct);
+
+		if (agreementValidation !== undefined) {
+			req.flash('error', {
+				title: 'There was a problem submitting your membership request:',
+				list: agreementValidation.messages
+			});
+			locals.validationErrors = agreementValidation.errors;
+			next();
+			return;
+		}
+
+		updater.process(newUser, {
+			flashErrors: true,
+			fields: 'name, email, membershipType',
+			errorMessage: 'There was a problem submitting your membership request:'
+		}, function(err) {
+			if (err) {
+				locals.validationErrors = err.errors;
+			} else {
+				locals.joinSubmitted = true;
+				newUser.sendCommitteeEmail();
+			}
+			next();
+		});
 	});
 
 	view.render('join');
-
 };
