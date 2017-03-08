46ddf42eb792d021d03d9710d0ed589fcd65c245
James Macfie <james@macfie.co.nz>
2017-03-08 15:05:02 +1300

    Add /help page

--- routes/index.js
+++ routes/index.js
@@ -48,6 +48,7 @@ exports = module.exports = function(app) {
   app.all('/contact', routes.views.contact);
   app.all('/join', routes.views.join);
 	app.all('/awards', routes.views.awards);
+	app.all('/help', routes.views.help);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
